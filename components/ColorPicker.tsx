7421f5b9915cd192074fe947af6f3662a17323ce
Robert McAbee <robertthemcabee@gmail.com>
2022-07-05 17:57:12 -0600

    quick fixes

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -88,6 +88,7 @@ function ColorPicker({
       <button
         className="hover:text-primary600 transition-colors row-span-2 w-12 h-full"
         onClick={handleToggle}
+        aria-label="show color picker"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -96,7 +97,7 @@ function ColorPicker({
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          strokeWidth={2}
+          strokeWidth={4}
         >
           <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
         </svg>
